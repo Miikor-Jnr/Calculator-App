fb2c2035981727f23003d3961c615d20b3e406e7
Miikor-Jnr <barilelomiikor@gmail.com>
2022-07-29 15:36:33 +0100

    This is a simple calculator for arithmetic operation and more

---
+++ calculator1.js
@@ -0,0 +1,11 @@
+// Selecting all the elements needed
+
+const screenDisplayPrevious = document.querySelector("[data-previous-operand]")
+const screenDisplayCurrent = document.querySelector("[data-current-operand]")
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationsButtons = document.querySelectorAll("[data-operation]")
+const equalButton = document.querySelector("[data-equals]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const deleteButton = document.querySelector("[data-delete]")
+
+console.log(typeof numberButtons)
